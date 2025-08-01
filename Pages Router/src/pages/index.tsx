bf51318d09e2514e71c15d8d1d5c3a763da83097
umsungjun <umseongjun@naver.com>
2025-08-01 21:23:15 +0900

    Feat: css module 방식 테스트

--- Pages Router/src/pages/index.tsx
+++ Pages Router/src/pages/index.tsx
@@ -1,3 +1,10 @@
+import style from "./index.module.css";
+
 export default function Home() {
-  return <h1>인덱스</h1>;
+  return (
+    <>
+      <h1 className={style.h1}>테스트1</h1>
+      <h2 className={style.h2}>테스트2</h2>
+    </>
+  );
 }
