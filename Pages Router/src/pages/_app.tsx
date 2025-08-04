9fe43655c885f249b15a7ac90a971067010c57b2
umsungjun <umseongjun@naver.com>
2025-08-04 22:11:49 +0900

    Feat: 페이지별 개별 레이아웃 적용할 수 있도록 처리

--- Pages Router/src/pages/_app.tsx
+++ Pages Router/src/pages/_app.tsx
@@ -1,11 +1,16 @@
+import { ReactNode } from "react";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import GlobalLayout from "@/components/GlobalLayout";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <GlobalLayout>
-      <Component {...pageProps} />
-    </GlobalLayout>
-  );
+interface MyAppProps extends AppProps {
+  Component: AppProps["Component"] & {
+    getLayout?: (page: ReactNode) => ReactNode;
+  };
+}
+
+export default function App({ Component, pageProps }: MyAppProps) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
