31cb41851ec936c2e03e833862c93a20f9bd90f3
umsungjun <umseongjun@naver.com>
2025-08-06 20:44:12 +0900

    Feat: 메인 페이지 초기 마크업 및 BookItem 컴포넌트 추가

--- Pages Router/src/pages/index.tsx
+++ Pages Router/src/pages/index.tsx
@@ -1,10 +1,30 @@
+import { ReactNode } from "react";
 import style from "./index.module.css";
+
+import SearchAbleLayout from "@/components/SearchAbleLayout";
+import BookItem from "@/components/BookItem";
+
+import books from "@/mock/books.json";
 
 export default function Home() {
   return (
-    <>
-      <h1 className={style.h1}>테스트1</h1>
-      <h2 className={style.h2}>테스트2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록 된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
   );
 }
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchAbleLayout>{page}</SearchAbleLayout>;
+};
