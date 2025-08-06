b62988e4419bb1ebbd706e1e0d5f8266ac560733
umsungjun <umseongjun@naver.com>
2025-08-06 20:56:24 +0900

    Feat: 도서 아이템 컴포넌트 생성 및 스타일링

---
+++ Pages Router/src/components/BookItem.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+import style from "./BookItem.module.css";
+import type { BookData } from "@/types";
+
+export default function BookItem({
+  id,
+  title,
+  subTitle,
+  description,
+  author,
+  publisher,
+  coverImgUrl,
+}: BookData) {
+  return (
+    <Link href={`/book/${id}`} className={style.container}>
+      <img src={coverImgUrl} alt="" />
+      <div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <br />
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+      </div>
+    </Link>
+  );
+}
