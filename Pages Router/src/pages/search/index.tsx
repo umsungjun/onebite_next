decd97c60e1c800b52948bf0e1cfa8ecb5d6b807
umsungjun <umseongjun@naver.com>
2025-08-06 21:17:05 +0900

    Feat: 검색 페이지 도서 아이템 컴포넌트 추가

--- Pages Router/src/pages/search/index.tsx
+++ Pages Router/src/pages/search/index.tsx
@@ -1,8 +1,18 @@
-import SearchAbleLayout from "@/components/SearchAbleLayout";
 import { ReactNode } from "react";
 
+import SearchAbleLayout from "@/components/SearchAbleLayout";
+import BookItem from "@/components/BookItem";
+
+import books from "@/mock/books.json";
+
 export default function Page() {
-  return <h1>Search</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
