f5236acd0244ebccb6d50a6d29bb8ffc93e2a2de
umsungjun <umseongjun@naver.com>
2025-08-11 21:47:51 +0900

    Feat: 메인 페이지 SSR 및 도서 목록 렌더링 구현
    - getServerSideProps로 추천 도서 및 전체 도서 데이터 병렬 fetching
    - Promise.all을 활용한 효율적인 데이터 로딩 처리

--- Pages Router/src/pages/index.tsx
+++ Pages Router/src/pages/index.tsx
@@ -1,23 +1,42 @@
 import { ReactNode } from "react";
+import { InferGetServerSidePropsType } from "next";
 import style from "./index.module.css";
+import { fetchBooks } from "@/lib/fetchBooks";
+import { fetchRandomBooks } from "@/lib/fetchRandomBooks";
 
 import SearchAbleLayout from "@/components/SearchAbleLayout";
 import BookItem from "@/components/BookItem";
 
-import books from "@/mock/books.json";
+export const getServerSideProps = async () => {
+  // Home 컾포넌트를 렌더링 하기 이전에 서버 사이드에서 데이터를 가져오는 함수
+  const [allBooks, randomBoos] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
 
-export default function Home() {
+  return {
+    props: {
+      allBooks,
+      randomBoos,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+  randomBoos,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {randomBoos.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록 된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
