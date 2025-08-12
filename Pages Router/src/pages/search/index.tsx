e2e889b51380bdc0225a41f0d79af0069f03d40d
umsungjun <umseongjun@naver.com>
2025-08-12 21:16:12 +0900

    Feat: 검색 페이지 SSR 및 도서 목록 렌더링 구현

--- Pages Router/src/pages/search/index.tsx
+++ Pages Router/src/pages/search/index.tsx
@@ -1,11 +1,29 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { ReactNode } from "react";
+import { fetchBooks } from "@/lib/fetchBooks";
 
 import SearchAbleLayout from "@/components/SearchAbleLayout";
 import BookItem from "@/components/BookItem";
 
-import books from "@/mock/books.json";
+/* 
+  - GetServerSidePropsContext 브라우저에서 전달 받는 Props를 정의하는 타입
+*/
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
 
-export default function Page() {
+  return {
+    props: {
+      books,
+    },
+  };
+};
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       {books.map((book) => (
