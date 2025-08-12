262504bb79e04abe49c825d381ec13dd98cd8e26
umsungjun <umseongjun@naver.com>
2025-08-12 21:39:15 +0900

    Feat: 도서 상세 조회 API 호출 함수 생성

---
+++ Pages Router/src/lib/fetchOneBook.ts
@@ -0,0 +1,18 @@
+import { BookData } from "@/types";
+
+export const fetchOneBook = async (id: number): Promise<BookData | null> => {
+  const url = `${process.env.API_BASE_URL}/book/${id}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error();
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+
+    return null;
+  }
+};
