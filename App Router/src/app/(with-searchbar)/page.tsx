cd650669c6c9ad8fe378c8e98969230399240718
umsungjun <umseongjun@naver.com>
2025-09-15 21:37:10 +0900

    Feat: 메인 페이지 SSR방식으로 API 호출해서 표시
    - 추천 도서
    - 등록 된 모든 도서

--- App Router/src/app/(with-searchbar)/page.tsx
+++ App Router/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,55 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
+
+async function AllBooks() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/book`);
+
+  if (res.ok === false) {
+    return <div>도서 정보를 불러오지 못했습니다.</div>;
+  }
+
+  const allBooks: BookData[] = await res.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+async function RecommendedBooks() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/book/random`
+  );
+
+  if (res.ok === false) {
+    return <div>도서 정보를 불러오지 못했습니다.</div>;
+  }
+
+  const recommendationBooks: BookData[] = await res.json();
+
+  return (
+    <div>
+      {recommendationBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecommendedBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
