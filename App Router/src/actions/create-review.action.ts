41c312e784c405492e6987857bf0b431d1dd9885
umsungjun <umseongjun@naver.com>
2025-12-25 21:52:20 +0900

    Feat: 상세 페이지 리뷰 등록 후 페이지 캐시 초기화 처리

--- App Router/src/actions/create-review.action.ts
+++ App Router/src/actions/create-review.action.ts
@@ -1,4 +1,7 @@
 "use server";
+
+import { revalidatePath } from "next/cache";
+
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
@@ -9,14 +12,14 @@ export async function createReviewAction(formData: FormData) {
   }
 
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/review`, {
+    await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/review`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ bookId, content, author }),
     });
-    console.log("res.status:", res.status);
+    revalidatePath(`/book/${bookId}`);
   } catch (err) {
     console.error("리뷰 작성 중 오류 발생:", err);
   }
